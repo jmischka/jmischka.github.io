9593a878808901ef14a6f696f8949717feb2a503
Mischka <jason.mischka@npgny2qrbf4g8wn.springernature.com>
2020-09-16 08:43:34 -0400

    final updates

--- src/index.js
+++ src/index.js
@@ -14,52 +14,74 @@ const backOverlays = Array.from(document.querySelectorAll('.backOverlay'));
 
 
 function handleDownload(e) {
-	e.preventDefault();
-	download.classList.toggle('active');			
+  e.preventDefault();
+  download.classList.toggle('active');
+  this.classList.toggle('active');
+  if (this.classList.contains('active')) {
+    this.setAttribute('aria-expanded', 'true');
+  } else {
+    this.setAttribute('aria-expanded', 'false');
+  }   
 }
 
 function handleDateClick(e) {
-	e.preventDefault();
-	let url = this.getAttribute('href');
-	let date = document.getElementById(url);
-	gsap.to(window, {duration: 3, scrollTo: {y: date, offsetY: 75}, ease: "power3.out"});
+  e.preventDefault();
+  let url = this.getAttribute('href');
+  let date = document.getElementById(url);
+  gsap.to(window, {duration: 3, scrollTo: {y: date, offsetY: 75}, ease: "power3.out"});
 }
 
 function handleScroll() {
-	let distanceScrolled = window.pageYOffset;
+  let distanceScrolled = window.pageYOffset;
 
-	if (!download.classList.contains('fixed') && distanceScrolled > (download.offsetTop + download.parentElement.offsetTop) - 75) {
-		download.classList.add('fixed');
-		download.firstElementChild.classList.add('fixed');
-	} else if (download.classList.contains('fixed') && distanceScrolled < (download.offsetTop + download.parentElement.offsetTop) - 75) {
-		download.classList.remove('fixed');
-		download.firstElementChild.classList.remove('fixed');
-	}
+  if (!download.classList.contains('fixed') && distanceScrolled > (download.offsetTop + download.parentElement.offsetTop) - 75) {
+    download.classList.add('fixed');
+    download.firstElementChild.classList.add('fixed');
+  } else if (download.classList.contains('fixed') && distanceScrolled < (download.offsetTop + download.parentElement.offsetTop) - 75) {
+    download.classList.remove('fixed');
+    download.firstElementChild.classList.remove('fixed');
+  }
 
-	if (!timelineWrapper.classList.contains('fixed') && distanceScrolled > (timelineWrapper.offsetTop + timelineWrapper.parentElement.offsetTop) - 75) {
-		timelineWrapper.classList.add('fixed');
-		timelineWrapper.firstElementChild.classList.add('fixed');
-	} else if (timelineWrapper.classList.contains('fixed') && distanceScrolled < (timelineWrapper.offsetTop + timelineWrapper.parentElement.offsetTop) - 75) {
-		timelineWrapper.classList.remove('fixed');
-		timelineWrapper.firstElementChild.classList.remove('fixed');
-	}
+  if (!timelineWrapper.classList.contains('fixed') && distanceScrolled > (timelineWrapper.offsetTop + timelineWrapper.parentElement.offsetTop) - 75) {
+    timelineWrapper.classList.add('fixed');
+    timelineWrapper.firstElementChild.classList.add('fixed');
+  } else if (timelineWrapper.classList.contains('fixed') && distanceScrolled < (timelineWrapper.offsetTop + timelineWrapper.parentElement.offsetTop) - 75) {
+    timelineWrapper.classList.remove('fixed');
+    timelineWrapper.firstElementChild.classList.remove('fixed');
+  }
 
-	milestones.forEach((milestone,index) => {
-		if (!milestone.classList.contains('scrolled') && index == 0 && distanceScrolled > milestone.offsetTop + milestone.parentElement.offsetTop + milestone.parentElement.offsetParent.offsetTop - (window.innerHeight / 2)) {
-			milestone.classList.add('scrolled');
-			timelineDates[index].classList.add('active');
-		} else if (!milestone.classList.contains('scrolled') && distanceScrolled > milestone.offsetTop + milestone.parentElement.offsetTop + milestone.parentElement.offsetParent.offsetTop - (window.innerHeight / 2)) {
-			let activeMarker = document.querySelector('.timeline-date.active');
-			milestone.classList.add('scrolled');
-			activeMarker.classList.remove('active');
-			timelineDates[index].classList.add('active');
-		} else if (milestone.classList.contains('scrolled') && distanceScrolled < milestone.offsetTop + milestone.parentElement.offsetTop + milestone.parentElement.offsetParent.offsetTop - (window.innerHeight / 2)) {
-			let activeMarker = document.querySelector('.timeline-date.active');
-			milestone.classList.remove('scrolled');
-			activeMarker.classList.remove('active');
-			timelineDates[index - 1].classList.add('active');
-		}
-	})
+  milestones.forEach((milestone,index) => {
+    if (!milestone.classList.contains('scrolled') && index == 0 && distanceScrolled > milestone.offsetTop + milestone.parentElement.offsetTop + milestone.parentElement.offsetParent.offsetTop - (window.innerHeight / 2)) {
+      let ariaCurrent = document.createAttribute('aria-current');
+      ariaCurrent.value = 'date';
+      milestone.classList.add('scrolled');
+      timelineDates[index].classList.add('active');
+      timelineDates[index].setAttributeNode(ariaCurrent);
+    } else if (!milestone.classList.contains('scrolled') && index !== 0 && distanceScrolled > milestone.offsetTop + milestone.parentElement.offsetTop + milestone.parentElement.offsetParent.offsetTop - (window.innerHeight / 2)) {
+      let activeMarker = document.querySelector('.timeline-date.active');
+      let ariaCurrent = document.createAttribute('aria-current');
+      ariaCurrent.value = 'date';
+      milestone.classList.add('scrolled');
+      activeMarker.classList.remove('active');
+      activeMarker.removeAttribute('aria-current');
+      timelineDates[index].classList.add('active');
+      timelineDates[index].setAttributeNode(ariaCurrent);
+    } else if (milestone.classList.contains('scrolled') && index !== 0 && distanceScrolled < milestone.offsetTop + milestone.parentElement.offsetTop + milestone.parentElement.offsetParent.offsetTop - (window.innerHeight / 2)) {
+      let activeMarker = document.querySelector('.timeline-date.active');
+      let ariaCurrent = document.createAttribute('aria-current');
+      ariaCurrent.value = 'date';
+      milestone.classList.remove('scrolled');
+      activeMarker.classList.remove('active');
+      activeMarker.removeAttribute('aria-current');
+      timelineDates[index - 1].classList.add('active');
+      timelineDates[index - 1].setAttributeNode(ariaCurrent);
+    } else if (milestone.classList.contains('scrolled') && index == 0 && distanceScrolled < milestone.offsetTop + milestone.parentElement.offsetTop + milestone.parentElement.offsetParent.offsetTop - (window.innerHeight / 2)) {
+      let activeMarker = document.querySelector('.timeline-date.active');
+      milestone.classList.remove('scrolled');
+      activeMarker.classList.remove('active');
+      activeMarker.removeAttribute('aria-current');
+    }
+  })
 
   backgroundTriggers.forEach((trigger,index) => {
       if (!trigger.classList.contains('triggered') && distanceScrolled > trigger.offsetTop + trigger.parentElement.offsetTop + trigger.parentElement.offsetParent.offsetTop) {
@@ -75,7 +97,7 @@ function handleScroll() {
 window.addEventListener('scroll', handleScroll);
 
 for (let i = 0; i < timelineDates.length; i++) {
-	timelineDates[i].addEventListener('click', handleDateClick);
+  timelineDates[i].addEventListener('click', handleDateClick);
 }
 
 downloadTrigger.addEventListener('click', handleDownload);
@@ -243,5 +265,3 @@ gsap.to("#t-cell-b", {
     scrub: true
   }
 });
-
-
